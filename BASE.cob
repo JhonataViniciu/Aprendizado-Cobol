ec3bc0249221e4051cc6504735eb47461015507c
Jhoww_24 <161961436+JhonataViniciu@users.noreply.github.com>
2024-11-02 17:37:09 -0300

    Add files via upload

---
+++ BASE.cob
@@ -0,0 +1,29 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+      *=======================================================
+      *== AUTOR: JHONATA                 EMPRESA:XPTO
+      *== OBJETIVO:
+      *== DATA: XX/XX/20XX
+      *== OBSERVAÇÕES:
+       ENVIRONMENT                     DIVISION.
+       CONFIGURATION                   SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+
+       DATA DIVISION.
+       WORKING-STORAGE                 SECTION.
+
+
+
+       PROCEDURE DIVISION.
+           STOP RUN.
+       0001-PRINCIPAL                  SECTION.
+       0100-INICIALIZAR                SECTION.
+
+
+
+       0200-PROCESSAR                  SECTION.
+
+
+
+       0300-FINALIZAR                  SECTION.
